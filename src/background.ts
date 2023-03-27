d883f472dff8ae4df14d321929b4fc8a0d5b8470
tomarin <tomarint@gmail.com>
2023-03-28 01:37:11 +0900

    Initialize the extension at chrome.runtime.onInstalled.addListener

--- src/background.ts
+++ src/background.ts
@@ -1,53 +1,48 @@
-(() => {
-  class MiterasKintaiHelperBackend {
-    readonly contextMenuId = "miteras-kintai-helper-context-menu-id";
-    readonly messageName = "miteras-kintai-helper-message";
+const contextMenuId = "miteras-kintai-helper-context-menu-id";
+const messageName = "miteras-kintai-helper-message";
 
-    constructor() {
-      chrome.contextMenus.create(
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create(
+    {
+      id: contextMenuId,
+      title: chrome.i18n.getMessage("extName"),
+      contexts: ["all"],
+      documentUrlPatterns: ["https://kintai.miteras.jp/*/work-condition"],
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError);
+      }
+    }
+  );
+});
+
+// Fired when a context menu item is clicked.
+chrome.contextMenus.onClicked.addListener(
+  (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
+    // console.log('context menu clicked');
+    // console.log(info);
+    // console.log(tab);
+    if (tab == null || tab.id == null || tab.id < 0) {
+      return;
+    }
+    if (info.menuItemId === contextMenuId) {
+      chrome.tabs.sendMessage(
+        tab.id,
         {
-          id: this.contextMenuId,
-          title: chrome.i18n.getMessage("extName"),
-          contexts: ["all"],
-          documentUrlPatterns: ["https://kintai.miteras.jp/*/work-condition"],
+          message: messageName,
         },
-        () => {
+        (response) => {
           if (chrome.runtime.lastError) {
             console.log(chrome.runtime.lastError);
-          }
-        }
-      );
-
-      // Fired when a context menu item is clicked.
-      chrome.contextMenus.onClicked.addListener(
-        (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
-          // console.log('context menu clicked');
-          // console.log(info);
-          // console.log(tab);
-          if (tab == null || tab.id == null || tab.id < 0) {
             return;
           }
-          if (info.menuItemId === this.contextMenuId) {
-            chrome.tabs.sendMessage(
-              tab.id,
-              {
-                message: this.messageName,
-              },
-              (response) => {
-                if (chrome.runtime.lastError) {
-                  console.log(chrome.runtime.lastError);
-                  return;
-                }
-                if (response != null && response.message === "success") {
-                  // console.log("contextMenu is succeeded.");
-                }
-              }
-            );
-            return true;
+          if (response != null && response.message === "success") {
+            // console.log("contextMenu is succeeded.");
           }
         }
       );
+      return true;
     }
   }
-  new MiterasKintaiHelperBackend();
-})();
+);
